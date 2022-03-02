9ba45c1c0f7f27d77ac5b37fcba230a092716518
Rina Andriana <rina@andriana.no>
2022-03-02 20:31:01 +0100

    Add Swap.sol

---
+++ contracts/Swap.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.0;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "./IWETH9.sol";
+
+contract Swap {
+    ISwapRouter public immutable swapRouter;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    uint24 public constant poolFee = 3000;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;        
+    }
+
+    function swapETH() payable external returns (uint256 amountOut) {                
+        IWETH9(WETH9).deposit{value: msg.value}();
+        
+        TransferHelper.safeApprove(WETH9, address(swapRouter), msg.value);
+
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: WETH9,
+                tokenOut: DAI,
+                fee: 3000,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: msg.value,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = swapRouter.exactInputSingle(params);
+    }
+}
