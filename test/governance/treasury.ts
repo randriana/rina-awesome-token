44cdabd75e332b223b94b54dfff0e91a6e2ab5f3
Rina Andriana <rina@andriana.no>
2022-03-11 13:34:39 +0100

    update treasury test

--- test/governance/treasury.ts
+++ test/governance/treasury.ts
@@ -1,13 +1,9 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { fromEther, toEther } from "../utils/format";
+import { fromEther } from "../utils/format";
 import { Token, GovernanceToken, ReleaseFund, Treasury } from "../../typechain";
 
 describe("Treasury", function () {
-  let owner: SignerWithAddress;
-  let hordeAccount: SignerWithAddress;
-  let withdrawer: SignerWithAddress;
   let governanceToken: GovernanceToken;
   let token: Token;
   let releaseFundMaster: ReleaseFund;
@@ -23,10 +19,9 @@ describe("Treasury", function () {
     const ReleaseFundMaster = await ethers.getContractFactory("ReleaseFund");
     const Token = await ethers.getContractFactory("Token");
     const Treasury = await ethers.getContractFactory("Treasury");
-    [owner, hordeAccount, withdrawer] = await ethers.getSigners();
 
     token = <Token>(
-      await Token.deploy("MyToken", "MT", fromEther(TOTAL_TOKEN_AMOUNT))
+      await Token.deploy("Token", "TK", fromEther(TOTAL_TOKEN_AMOUNT))
     );
     governanceToken = <GovernanceToken>(
       await GovernanceToken.deploy(fromEther(TOTAL_GOV_TOKEN_AMOUNT))
@@ -74,5 +69,12 @@ describe("Treasury", function () {
     await expect(
       treasury.release(fromEther(RELEASE_AMOUNT))
     ).to.be.revertedWith("Amount exceeded limit");
+  });
+
+  it("Should throw error: <Amount exceeded balance>", async function () {
+    await (await treasury.setMaxReleaseAmount(fromEther(100_000_000))).wait();
+    await expect(treasury.release(fromEther(100_000_000))).to.be.revertedWith(
+      "Amount exceeded balance"
+    );
   });
 });
