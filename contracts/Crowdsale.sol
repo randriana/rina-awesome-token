c9aaba90fcfd296ab8afe3f37e4d78df8346bfce
Rina Andriana <rina@andriana.no>
2022-03-11 15:02:53 +0100

    Send minting fee to treasury contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -13,10 +13,10 @@ import "hardhat/console.sol";
 /**
  * @title Crowdsale
  * @dev Crowdsale is a base contract for managing a token crowdsale,
- * allowing investors to purchase tokens with ether. This contract implements
+ * allowing investors to purchase amountToMint with ether. This contract implements
  * such functionality in its most fundamental form and can be extended to provide additional
  * functionality and/or custom behavior.
- * The external interface represents the basic interface for purchasing tokens, and conforms
+ * The external interface represents the basic interface for purchasing amountToMint, and conforms
  * the base architecture for crowdsales. It is *not* intended to be modified / overridden.
  * The internal interface conforms the extensible and modifiable surface of crowdsales. Override
  * the methods to add functionality. Consider using 'super' where appropriate to concatenate
@@ -34,18 +34,22 @@ contract Crowdsale is Context, ReentrancyGuard, AccessControl{
 
     // Amount of wei raised
     uint256 private _weiRaised;
-
     uint256 private _rate;
+    uint256 private _mintingFee;
 
     bytes32 public constant MAINTAINER_ROLE = keccak256("MAINTAINER_ROLE");
+
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public treasury;
+
     Swap private _swap;
+
 
     /**
      * Event for token purchase logging
-     * @param purchaser who paid for the tokens     
+     * @param purchaser who paid for the amountToMint     
      * @param value weis paid for purchase
-     * @param amount amount of tokens purchased
+     * @param amount amount of amountToMint purchased
      */
     event TokensMinted(address indexed purchaser, uint256 value, uint256 amount);
 
@@ -57,23 +61,55 @@ contract Crowdsale is Context, ReentrancyGuard, AccessControl{
      * @param token Address of the token being sold
      * @param initialRate Initial rate for token price
      */
-    constructor (address payable wallet, Token token, uint256 initialRate, Swap swap){        
+    constructor (address payable wallet, Token token, uint256 initialRate, Swap swap, uint256 mintingFee, address _treasury){        
         require(wallet != address(0), "Crowdsale: wallet is the zero address");
         require(address(token) != address(0), "Crowdsale: token is the zero address");        
         require(initialRate > 0, "Rate cannot be 0");
+        require(treasury != address(0), "Treasury is zero address");
         
         _wallet = wallet;
         _token = token;        
         _rate = initialRate;
         _swap = swap;
+        _mintingFee = mintingFee;
+        treasury = _treasury;
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     /**
+     * @param beneficiary Recipient of the token purchase
+     */
+    function buyTokens(address beneficiary) external nonReentrant payable {        
+        _preValidatePurchase(beneficiary, msg.value);
+
+        uint256 swappedAmount = _swapETH(msg.value);
+
+        _forwardFunds(swappedAmount);
+        
+        uint256 amountToMint = _getTokenAmount(swappedAmount);
+        uint256 mintingFee = _calculateFee(amountToMint);
+        
+        amountToMint -= mintingFee;
+
+        // update state
+        _weiRaised = _weiRaised.add(swappedAmount);
+
+        _mint(treasury, mintingFee);
+        _mint(beneficiary, amountToMint);
+
+        emit TokensMinted(_msgSender(), swappedAmount, amountToMint);
+
+        _updatePurchasingState(beneficiary, swappedAmount);
+
+        _postValidatePurchase(beneficiary, swappedAmount);
+    }
+
+
+    /**
      * Note that other contracts will transfer funds with a base gas stipend
      * of 2300, which is not enough to call buyTokens. Consider calling
-     * buyTokens directly when purchasing tokens from a contract.
+     * buyTokens directly when purchasing amountToMint from a contract.
      */
     receive () external payable {}
 
@@ -110,32 +146,6 @@ contract Crowdsale is Context, ReentrancyGuard, AccessControl{
     }    
 
     /**
-     * @dev low level token purchase ***DO NOT OVERRIDE***
-     * This function has a non-reentrancy guard, so it shouldn't be called by
-     * another `nonReentrant` function.
-     * @param beneficiary Recipient of the token purchase
-     */
-    function buyTokens(address beneficiary) external nonReentrant payable {        
-        _preValidatePurchase(beneficiary, msg.value);
-
-        uint256 swappedAmount = _swapETH(msg.value);
-
-        _forwardFunds(swappedAmount);
-        
-        uint256 tokens = _getTokenAmount(swappedAmount);
-
-        // update state
-        _weiRaised = _weiRaised.add(swappedAmount);
-
-        _mint(beneficiary, tokens);
-        emit TokensMinted(_msgSender(), swappedAmount, tokens);
-
-        _updatePurchasingState(beneficiary, swappedAmount);
-
-        _postValidatePurchase(beneficiary, swappedAmount);
-    }
-
-    /**
      * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met.
      * Use `super` in contracts that inherit from Crowdsale to extend their validations.
      * Example from CappedCrowdsale.sol's _preValidatePurchase method:
@@ -163,7 +173,7 @@ contract Crowdsale is Context, ReentrancyGuard, AccessControl{
     /**
      * @dev Override for extensions that require an internal state to check for validity (current user contributions,
      * etc.)
-     * @param beneficiary Address receiving the tokens
+     * @param beneficiary Address receiving the amountToMint
      * @param weiAmount Value in wei involved in the purchase
      */
     function _updatePurchasingState(address beneficiary, uint256 weiAmount) internal virtual {
@@ -171,9 +181,9 @@ contract Crowdsale is Context, ReentrancyGuard, AccessControl{
     }
 
     /**
-     * @dev Override to extend the way in which ether is converted to tokens.
-     * @param weiAmount Value in wei to be converted into tokens
-     * @return Number of tokens that can be purchased with the specified _weiAmount
+     * @dev Override to extend the way in which ether is converted to amountToMint.
+     * @param weiAmount Value in wei to be converted into amountToMint
+     * @return Number of amountToMint that can be purchased with the specified _weiAmount
      */
     function _getTokenAmount(uint256 weiAmount) private view returns (uint256) {
         return weiAmount.mul(_rate) / 1 ether;
@@ -192,5 +202,9 @@ contract Crowdsale is Context, ReentrancyGuard, AccessControl{
 
     function _swapETH(uint256 amount) private returns (uint256) {
         return _swap.swapETH{ value: amount}();
+    }
+
+    function _calculateFee(uint256 amount) private view returns (uint256) {
+        return amount * _mintingFee;
     }
 }
